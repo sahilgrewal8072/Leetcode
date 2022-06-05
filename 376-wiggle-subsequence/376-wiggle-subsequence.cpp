c3ada420b67956fc9a5012614813cd4ae9896d50
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-05 18:57:22 +0530

    Time: 4 ms (52.78%), Space: 7.2 MB (42.40%) - LeetHub

--- 376-wiggle-subsequence/376-wiggle-subsequence.cpp
+++ 376-wiggle-subsequence/376-wiggle-subsequence.cpp
@@ -18,21 +18,18 @@ public:
             if(nums[0] == nums[1])return 1;
             return 2;
         }
-        vector<vector<int>> dp(n+1, vector<int>(2, 0));
-        // int mx = 0;
-        // for(int i=0; i<=1; i++){
-        //     mx = max(mx, solve(1, i, nums, dp, n));
-        // }
-        // return mx + 1;
+        // vector<vector<int>> dp(n+1, vector<int>(2, 0));
+       vector<int> nxt(2, 0), curr(2, 0);
         for(int ind = n-1; ind>0; ind--){
             for(int state = 0; state <= 1; state++){
                 int take = 0;
                 if(nums[ind] - nums[ind-1] < 0 && state == 1 || nums[ind] - nums[ind-1] > 0 && state == 0){
-                    dp[ind][state] =  1 + dp[ind + 1][!state];
+                    curr[state] =  1 + nxt[!state];
                 }else
-                dp[ind][state] = 0 + dp[ind + 1][state];
+                curr[state] = 0 + nxt[state];
             }
+            nxt = curr;
         }
-        return 1 + max(dp[1][1], dp[1][0]);
+        return 1 + max(nxt[1], nxt[0]);
     }
 };
