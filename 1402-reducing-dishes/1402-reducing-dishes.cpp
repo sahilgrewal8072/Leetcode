97d4f62807cb9790ef0461c91f4cee8de3203847
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-06 22:50:15 +0530

    Time: 134 ms (5.05%), Space: 20.9 MB (10.19%) - LeetHub

--- 1402-reducing-dishes/1402-reducing-dishes.cpp
+++ 1402-reducing-dishes/1402-reducing-dishes.cpp
@@ -4,6 +4,7 @@ public:
         if(ind == n){
             return 0;
         }
+        
         if(dp[ind][time] != -1)return dp[ind][time];
         int take =  arr[ind]*time + solve(ind + 1, time + 1, arr, dp, n);
         int notake = solve(ind + 1, time, arr, dp, n);
@@ -13,7 +14,15 @@ public:
     int maxSatisfaction(vector<int>& arr) {
         int n = arr.size();
         sort(arr.begin(), arr.end());
-        vector<vector<int>> dp(n, vector<int>(n+2, -1));
-        return solve(0, 1, arr, dp, n);
+    vector<vector<int>> dp(n+1, vector<int>(n+2, 0));
+        // return solve(0, 1, arr, dp, n);
+        for(int ind=n-1; ind>=0; ind--){
+            for(int time = n ; time>=1; time--){
+                 int take =  arr[ind]*time + dp[ind + 1][time + 1];
+                int notake = dp[ind + 1][time];
+                dp[ind][time] = max(take, notake);
+            }
+        }
+        return dp[0][1];
     }
 };
