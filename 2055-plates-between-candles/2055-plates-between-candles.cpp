423d40aacd38d813b5708548feb1a229a3505882
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-10 20:04:13 +0530

    Time: 865 ms (16.60%), Space: 163.1 MB (12.77%) - LeetHub

--- 2055-plates-between-candles/2055-plates-between-candles.cpp
+++ 2055-plates-between-candles/2055-plates-between-candles.cpp
@@ -2,22 +2,46 @@ class Solution {
 public:
     vector<int> platesBetweenCandles(string s, vector<vector<int>>& queries) {
         int n = s.length();
-        vector<int> arr;
+        vector<int> prev(n, n), nxt(n, n), prefixSum(n, 0);
         for(int i=0; i<n; i++){
-            if(s[i] == '|'){
-                arr.push_back(i);
+            if(i == 0){
+                if(s[i] == '|')prev[i] = i;
+                else
+                prefixSum[i] = 1;
+            }else{
+                if(s[i] == '|'){
+                    prev[i] = i;
+                    prefixSum[i] = prefixSum[i-1];
+                }else{
+                    prev[i] = prev[i-1];
+                    prefixSum[i] = 1 + prefixSum[i-1];
+                }
             }
         }
+        
+        for(int i = n-1; i>=0; i--){
+            if(i == n-1){
+                if(s[i] == '|'){
+                    nxt[i] = i;
+                }
+            }else{
+                if(s[i] == '|'){
+                    nxt[i] = i;
+                }else{
+                    nxt[i] = nxt[i+1];
+                }
+            }   
+        }
+        
         vector<int> ans;
         for(auto it : queries){
             int low = it[0];
             int high = it[1];
-            int l_ind = lower_bound(arr.begin(), arr.end(), low) - arr.begin();
-            int h_ind = upper_bound(arr.begin(), arr.end(), high) - arr.begin();
-            h_ind--;
-            if(l_ind >= h_ind) ans.push_back(0);
+            int i = nxt[low];
+            int j = prev[high];
+            if(i>=j || i == n || j == n)ans.push_back(0);
             else
-            ans.push_back(arr[h_ind] - arr[l_ind] - (h_ind - l_ind));
+                ans.push_back((prefixSum[j] - prefixSum[i]));
         }
         return ans;
     }
