36d90e4e5fe26e05db3529420ec799e77fb96e66
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-13 01:16:58 +0530

    Time: 647 ms (83.74%), Space: 235.5 MB (77.83%) - LeetHub

--- 2080-range-frequency-queries/2080-range-frequency-queries.cpp
+++ 2080-range-frequency-queries/2080-range-frequency-queries.cpp
@@ -1,7 +1,7 @@
 class RangeFreqQuery {
 public:
     unordered_map<int, vector<int>> mp;
-    unordered_map<string, int> mp2;
+
     RangeFreqQuery(vector<int>& arr) {
         int n = arr.size();
         for(int i=0; i<n; i++){
@@ -10,15 +10,15 @@ public:
     }
     
     int query(int left, int right, int value) {
-        string s = to_string(left) + '$' + to_string(right) + '$' + to_string(value);
-        if(mp2.find(s) != mp2.end())return mp2[s];
+        // string s = to_string(left) + '$' + to_string(right) + '$' + to_string(value);
+        // if(mp2.find(s) != mp2.end())return mp2[s];
         vector<int> &arr = mp[value];
         int count = 0;
-        int low = upper_bound(arr.begin(), arr.end(), left) - arr.begin();
-        if(low-1 >= 0 && arr[low-1] == left)count++;
+        int low = lower_bound(arr.begin(), arr.end(), left) - arr.begin();
+        // if(low-1 >= 0 && arr[low-1] == left)count++;
         int high = upper_bound(arr.begin(), arr.end(), right) - arr.begin();
-        count += high - low;
-        mp2[s] = count;
+        count = high - low;
+        // mp2[s] = count;
         return count;
     }
 };
