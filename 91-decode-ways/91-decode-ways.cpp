b2a0e9a1619a6d49fc607f23868c085bf40f1d33
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-14 23:57:07 +0530

    Time: 0 ms (100.00%), Space: 6.3 MB (44.05%) - LeetHub

--- 91-decode-ways/91-decode-ways.cpp
+++ 91-decode-ways/91-decode-ways.cpp
@@ -19,7 +19,21 @@ public:
     
     int numDecodings(string s) {
         int n = s.length();
-        vector<int> dp(n, -1);
-        return solve(0, s, dp, n);
+        vector<int> dp(n+1, 0);
+        // return solve(0, s, dp, n);
+        dp[n] = 1;
+        for(int ind=n-1; ind>=0; ind--){
+            if(ind < n && s[ind] == '0'){
+                dp[ind] = 0;
+                continue;
+            }
+             int ways = dp[ind + 1];
+            // pick pairs
+            if(ind + 1 < n && ((s[ind] == '1' && s[ind+1] <= '9') || (s[ind] == '2' && s[ind+1] < '7'))){
+                ways += dp[ind + 2];
+            }
+             dp[ind] = ways;
+        }
+        return dp[0];
     }
 };
