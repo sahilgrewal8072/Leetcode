3d7eff54362a6ed7415e036499bc3c6da1077e4d
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-14 22:30:55 +0530

    Time: 13 ms (98.44%), Space: 10.4 MB (93.33%) - LeetHub

---
+++ 2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses/2086-minimum-number-of-buckets-required-to-collect-rainwater-from-houses.cpp
@@ -0,0 +1,25 @@
+class Solution {
+public:
+    int minimumBuckets(string s) {
+        int n = s.length();
+        int ans = 0;
+        for(int i=0; i < n ;){
+            if(s[i] == 'H'){
+                if(i + 1 < n && s[i+1] == '.'){
+                ans ++;
+                i += 3;
+                    continue;
+            } 
+                if(i - 1 >= 0 && s[i-1] == '.'){
+                ans ++;
+                // i++;
+                }else{
+                    return -1;
+                }
+            }
+            i++;
+
+        }
+        return ans;
+    }
+};
