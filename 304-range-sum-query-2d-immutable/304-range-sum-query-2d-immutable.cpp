21f71e717489ef37a8be8e93ef096510614f4230
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-15 22:47:36 +0530

    Time: 865 ms (17.11%), Space: 142.4 MB (99.77%) - LeetHub

---
+++ 304-range-sum-query-2d-immutable/304-range-sum-query-2d-immutable.cpp
@@ -0,0 +1,49 @@
+class NumMatrix {
+public:
+    vector<vector<int>> dp;
+    NumMatrix(vector<vector<int>>& matrix) {
+        int n = matrix.size();
+        int m = matrix[0].size();
+          dp.resize(n, vector<int>(m));
+        for(int i=0; i<n; i++){
+             dp[i][0] = matrix[i][0];
+            for(int j=1; j<m; j++){
+                dp[i][j] = dp[i][j-1] + matrix[i][j];
+            }
+        }
+        
+        // storing the prefix sum col wise
+        
+        // for(int i=0; i<m; i++){
+        //     vector<int> temp(n, 0);
+        //      temp[0] = matrix[0][i];
+        //     for(int j=1; j<n; j++){
+        //         temp[j] = temp[j-1] + matrix[j][i];
+        //     }
+        //     mp2[i] = temp;
+        // }
+        
+    }
+    
+    int sumRegion(int row1, int col1, int row2, int col2) {
+        
+        int row_start = col1;
+        int row_end = col2;
+        int col_start = row1;
+        int col_end = row2;
+        long long int sum = 0;
+        for(int r = row1; r <= row2; r++){
+            if(col1 - 1 < 0 )sum += dp[r][col2];
+            else{
+                sum += dp[r][col2] - dp[r][col1-1];
+            }
+        }
+        return sum;
+    }
+};
+
+/**
+ * Your NumMatrix object will be instantiated and called as such:
+ * NumMatrix* obj = new NumMatrix(matrix);
+ * int param_1 = obj->sumRegion(row1,col1,row2,col2);
+ */
