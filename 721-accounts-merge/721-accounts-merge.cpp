070b2ffa0f3f044cc4a722412b33156f9f0b4556
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-17 17:21:12 +0530

    Time: 176 ms (53.78%), Space: 33.4 MB (86.80%) - LeetHub

--- 721-accounts-merge/721-accounts-merge.cpp
+++ 721-accounts-merge/721-accounts-merge.cpp
@@ -1,46 +1,84 @@
-class Solution {
+class DSU {
 public:
-    unordered_set<string> visited;
-    unordered_map<string, vector<string>> adj;
+    vector<int> representative;
+    vector<int> size;
     
-    
-    void dfs(string &email, vector<string> &curr_acc){
-        visited.insert(email);
-        
-        curr_acc.push_back(email);
-        for(auto it : adj[email]){
-            if(visited.find(it) == visited.end()){
-                dfs(it, curr_acc);
-            }
+    DSU(int sz) : representative(sz), size(sz) {
+        for (int i = 0; i < sz; ++i) {
+            // Initially each group is its own representative
+            representative[i] = i;
+            // Intialize the size of all groups to 1
+            size[i] = 1;
         }
     }
     
+    // Finds the representative of group x
+    int findRepresentative(int x) {
+        if (x == representative[x]) {
+            return x;
+        }
+        
+        // This is path compression
+        return representative[x] = findRepresentative(representative[x]);
+    }
+    
+    // Unite the group that contains "a" with the group that contains "b"
+    void unionBySize(int a, int b) {
+        int representativeA = findRepresentative(a);
+        int representativeB = findRepresentative(b);
+        
+        // If nodes a and b already belong to the same group, do nothing.
+        if (representativeA == representativeB) {
+            return;
+        }
+        
+        // Union by size: point the representative of the smaller
+        // group to the representative of the larger group.
+        if (size[representativeA] >= size[representativeB]) {
+            size[representativeA] += size[representativeB];
+            representative[representativeB] = representativeA;
+        } else {
+            size[representativeB] += size[representativeA];
+            representative[representativeA] = representativeB;
+        }
+    }
+};
+
+class Solution {
+public:
     vector<vector<string>> accountsMerge(vector<vector<string>>& accountList) {
-        for(vector<string>& account : accountList){
-            int accountSize = account.size();
-            string firstEmail = account[1];
-            for(int j=2; j<accountSize; j++){
-                string email = account[j];
-                adj[firstEmail].push_back(email);
-                adj[email].push_back(firstEmail);
+        int accountListSize = accountList.size();
+                DSU dsu(accountListSize);
+
+        unordered_map<string, int> emailGrp;
+        for(int i=0; i<accountListSize; i++){
+            int accountSize = accountList[i].size();
+            for(int j=1; j<accountSize; j++){
+                string email = accountList[i][j];
+                string holder = accountList[i][0];
+                if(emailGrp.find(email) == emailGrp.end()){
+                    emailGrp[email] = i;
+                }else{
+                    dsu.unionBySize(i, emailGrp[email]);
+                }
             }
         }
         
-        
-        vector<vector<string>> ans;
-        for(vector<string> &account : accountList){
-            string holder = account[0];
-            string firstEmail = account[1];
-            
-            if(visited.find(firstEmail) == visited.end()){
-                vector<string> curr_acc;
-                curr_acc.push_back(holder);
-                dfs(firstEmail, curr_acc);
-                
-                sort(curr_acc.begin() + 1, curr_acc.end());
-                ans.push_back(curr_acc);
-            }
+        unordered_map<int, vector<string>> components;
+        for(auto emailIter : emailGrp){
+            string email = emailIter.first;
+            int grp = emailIter.second;
+            components[dsu.findRepresentative(grp)].push_back(email);
         }
-        return ans;
+        
+        vector<vector<string>> mergedAcc;
+        for(auto componentIt : components){
+            int grp = componentIt.first;
+            vector<string> component = {accountList[grp][0]};
+            component.insert(component.end(), componentIt.second.begin(), componentIt.second.end());
+            sort(component.begin() + 1, component.end());
+            mergedAcc.push_back(component);
+        }
+        return mergedAcc;
     }
 };
