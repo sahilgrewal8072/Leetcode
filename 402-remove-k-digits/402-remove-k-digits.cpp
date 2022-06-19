d301133df75df76e0264dc30d0a8ae766020718b
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-19 23:05:17 +0530

    Time: 18 ms (53.90%), Space: 9.7 MB (10.11%) - LeetHub

--- 402-remove-k-digits/402-remove-k-digits.cpp
+++ 402-remove-k-digits/402-remove-k-digits.cpp
@@ -9,26 +9,30 @@ public:
                 st.pop();
                 k--;
             }
-            
             st.push(num[i] - 0);
         }
+        
         string ans = "";
         while(!st.empty()){
             ans += st.top();
             st.pop();
         }
-        // if(ans.length() == 1 && ans[0] == '0')return "0";
+      
         reverse(ans.begin(), ans.end());
         int i=0;
+        
+        //removal of leading zeros
         for(i=0; i<ans.length(); i++){
             if(ans[i] != '0')break;
         }
+        
+        // If there is still chance for removal i.e k > 0
         while(k-- && !ans.empty()){
-            // cout<<"hi";
             ans.pop_back();
         }
         
         if(ans.length() == 1 && ans[0] == '0' || ans.length() == i)return "0";
+        
         return ans.substr(i, ans.length());
     }
 };
