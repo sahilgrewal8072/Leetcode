9a7d537cea408427a31d50dc1198ea259e990078
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-19 19:04:02 +0530

    Time: 560 ms (74.41%), Space: 120 MB (97.62%) - LeetHub

--- 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.cpp
+++ 2106-maximum-fruits-harvested-after-at-most-k-steps/2106-maximum-fruits-harvested-after-at-most-k-steps.cpp
@@ -1,44 +1,35 @@
 class Solution {
+    
+    private:
+    bool isValid(int startPos, int left, int right, int k){
+        if(right <= startPos){
+            return (startPos - left <= k);
+        }else if(left >= startPos){
+            return (right - startPos <= k);
+        }else{
+            int x = startPos - left;
+            int y = right - startPos;
+            if(left <= right && (2*x + y <= k || 2*y + x <= k)){
+                return true;
+            }
+        }
+        return false;
+    }
+    
 public:
     int maxTotalFruits(vector<vector<int>>& fruits, int startPos, int k) {
-        vector<int> prefixSum(2e5 + 1, 0);
-        int n = 2e5 + 1;
-        for(int i=0; i<fruits.size(); i++){
-            prefixSum[fruits[i][0]] = fruits[i][1];
-        }
-        
-        for(int i=1; i<n; i++){
-            prefixSum[i] += prefixSum[i-1]; 
-        }
-        
         int maxi = 0;
+        int right = 0;
+        int n = fruits.size();
         int sum = 0;
-        // calculation for the right
-        for(int i = startPos; i<min(n-1, startPos + k); i++){
-            int r = i;
-            int x = i - startPos;
-            int l = startPos - (k - 2*x);
-            l = min(startPos, l);
-            sum = prefixSum[r];
-            if(l > 0){
-                sum -= prefixSum[l-1];
+        int left = 0;
+        while(right < n){
+            sum += fruits[right][1];
+            while(left <= right && !isValid(startPos, fruits[left][0], fruits[right][0], k)){
+                sum -= fruits[left++][1];
             }
             maxi = max(maxi, sum);
-        }
-        
-        
-        // calculation for the left
-        sum = 0;
-        for(int i = startPos; i>=max(0, startPos - k); i--){
-            int l = i;
-            int x = startPos - i;
-            int r = startPos + (k - 2*x);
-            r = max(startPos, min(r, n-1));
-            sum = prefixSum[r];
-            if(l > 0){
-                sum -= prefixSum[l-1];
-            }
-            maxi = max(maxi, sum);
+            right++;
         }
         return maxi;
     }
