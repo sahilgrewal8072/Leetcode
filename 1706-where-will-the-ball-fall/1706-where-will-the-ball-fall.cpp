d8f95cd455de611c399623dbb8e66f98f09147f7
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-22 15:20:28 +0530

    Time: 32 ms (62.82%), Space: 14.2 MB (15.76%) - LeetHub

---
+++ 1706-where-will-the-ball-fall/1706-where-will-the-ball-fall.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    private:
+    bool isValid(int nx, int ny, int m, int n){
+        if(nx < 0 || ny < 0 || nx >= m || ny >= n)return false; 
+        return true;
+    }
+    
+public:
+    
+    int solve(int x, int y, vector<vector<int>> &grid, int m, int n, vector<vector<int>> &dp){    
+        if(x == m){
+            return y;
+        }    
+        if(dp[x][y] != -2)return dp[x][y];
+        if(isValid(x, y, m, n)){
+            
+        }
+        if(grid[x][y] == 1){
+            if(isValid(x, y + 1, m, n) && grid[x][y+1] == 1){
+                return dp[x][y] = solve(x+1, y+1, grid, m, n, dp);
+            }
+        }else{
+            if(isValid(x, y-1, m, n) && grid[x][y-1] == -1){
+                return dp[x][y] = solve(x + 1, y - 1, grid, m, n, dp);
+            }
+        }
+        return dp[x][y] = -1;
+    }
+    
+    vector<int> findBall(vector<vector<int>>& grid) {
+        int m = grid.size();
+        int n = grid[0].size();
+        vector<vector<int>> dp(m, vector<int>(n, -2));
+        vector<int> ans(n, -1);
+        for(int i=0; i<n; i++){
+            ans[i] = solve(0, i, grid, m, n, dp);
+        }
+        return ans;
+    }
+};
