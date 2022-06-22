4b5b8f29db023e4ab8663fa0e35a601cc50a4d00
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-06-22 20:34:42 +0530

    Time: 658 ms (45.52%), Space: 27.2 MB (45.80%) - LeetHub

--- 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
+++ 2312-selling-pieces-of-wood/2312-selling-pieces-of-wood.cpp
@@ -12,7 +12,7 @@ class Solution {
         for(int h = 1; h < m; h++){
             price = max(price, solve(m-h, n, cost, dp) + solve(h, n, cost, dp));
         }
-        
+        // partition horizontally
         for(int w = 1; w < n; w++){
             price =  max(price, solve(m, n-w, cost, dp) + solve(m, w, cost, dp));
         }
@@ -24,12 +24,30 @@ public:
     long long sellingWood(int m, int n, vector<vector<int>>& prices) {
 
         vector<vector<int>> cost(m + 1, vector<int>(n + 1, 0));
-        vector<vector<long long>> dp(m+1, vector<long long>(n+1,-1));
+        vector<vector<long long>> dp(m+1, vector<long long>(n+1,0));
         for(auto &it : prices){
             int x = it[0];
             int y = it[1];
             cost[x][y] = it[2];
         }
-        return solve(m, n, cost, dp);
+        // return solve(m, n, cost, dp);
+        // tabulation
+        
+        for(int i = 1; i<=m; i++){
+            for(int j = 1; j<=n; j++){
+                long long  price = cost[i][j];
+                    // partition vertically
+                    for(int h = 1; h < i; h++){
+                         price = max(price, dp[i-h][j] + dp[h][j]);
+                    }
+                    // partition horizontally
+                    for(int w = 1; w < j; w++){
+                        price =  max(price, dp[i][j-w] + dp[i][w]);
+                    }
+                
+                 dp[i][j] = price;
+            }
+        }
+        return dp[m][n];
     }
 };
