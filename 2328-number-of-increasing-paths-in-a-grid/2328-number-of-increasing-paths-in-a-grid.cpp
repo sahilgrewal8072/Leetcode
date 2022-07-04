cde296656c38d15d7b2e3e42757ce4307a6f0382
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-05 00:07:52 +0530

    Time: 475 ms (50.00%), Space: 47.8 MB (100.00%) - LeetHub

--- 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
+++ 2328-number-of-increasing-paths-in-a-grid/2328-number-of-increasing-paths-in-a-grid.cpp
@@ -1,47 +1,33 @@
-int M = 1e9 + 7;
+const int M = 1e9 + 7;
 class Solution {  
-       private:
-    
-    int dfs(int i, int j, int prev, vector<vector<int>> &grid, int m, int n, int dx[], int dy[], vector<vector<long long>> &dp){
+private:
+    int dfs(int i, int j, vector<vector<int>> &grid, int m, int n, int dx[], int dy[], vector<vector<long long>> &dp){
         long long count = 1;
         if(dp[i][j] != -1)return dp[i][j];
         for(int ind=0; ind<4; ind++){
             int nx = i + dx[ind];
             int ny = j + dy[ind];
             if(nx >= 0 && ny >= 0 && nx < m && ny < n  && grid[nx][ny] > grid[i][j]){
-                count +=  (dfs(nx, ny, grid[i][j], grid, m, n, dx, dy, dp)%M)%M;
+                count +=  (dfs(nx, ny, grid, m, n, dx, dy, dp)%M)%M;
             }
-        }
-        
+        }      
         return dp[i][j] = count%M;
     }
-    
-    
-public:
-    
+        
+public:    
     int countPaths(vector<vector<int>>& matrix) {
         int m = matrix.size();
         int n = matrix[0].size();
-        int mx = 0;
         vector<vector<long long>> dp(m, vector<long long>(n, -1));
             int dx[] = {1, -1, 0, 0};
             int dy[] = {0, 0, 1, -1};
         long long sum = 0;
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
-                sum += dfs(i, j, -1, matrix, m, n, dx, dy, dp)%M;
+                sum += dfs(i, j, matrix, m, n, dx, dy, dp)%M;
                 sum %= M;
             }
-        }
-        
-        
-        // for(int i=0; i<m; i++){
-        //     for(int j=0; j<n; j++){
-        //         cout<<dp[i][j]<<" ";
-        //     }
-        //     cout<<endl;
-        // }
-        
+        }      
         return sum%M;
     }
 };
