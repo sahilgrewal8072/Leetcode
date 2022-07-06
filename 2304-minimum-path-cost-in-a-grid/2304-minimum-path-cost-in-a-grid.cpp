77b5d5975d3295ad8eb582df065f9346bc4a0770
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-06 15:23:27 +0530

    Time: 542 ms (45.93%), Space: 80.5 MB (45.05%) - LeetHub

--- 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
+++ 2304-minimum-path-cost-in-a-grid/2304-minimum-path-cost-in-a-grid.cpp
@@ -1,13 +1,14 @@
-class Solution {
-    
+class Solution {    
     private:
     int solve(int x, int y, vector<vector<int>> &grid, vector<vector<int>> &moveCost, int m, int n, int dy[], vector<vector<int>> &dp){
         if(x == m-1){
-            // return 0;
             return grid[x][y];    
         }
+        
         if(dp[x][y] != -1)return dp[x][y];
+        
         int mn = 1e9;
+    
         for(int i=0; i<=2*n; i++){
             int nx = x + 1;
             int ny = y + dy[i];
@@ -28,17 +29,21 @@ public:
         int j = 0;
         int dy[2*n + 1];
         int k = 1;
+        
         for(j=0; j<n; j++){
             dy[j] = -1*k;
             k++;
         }
-        dy[j] = 0;
-        j++;
+        
+        dy[j++] = 0;
          k = 1;
+        
         for(j; j<=2*n; j++)dy[j] = k++;
+        
         for(int i=0; i<n; i++){
             mn = min(mn, solve(0, i, grid, moveCost, m, n, dy, dp));
         }
+        
         return mn;
     }
 };
