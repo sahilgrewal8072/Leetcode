0481b00e2643b677115ec8f1e3b722c2859b7cc8
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-07 15:53:59 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (48.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -2,9 +2,9 @@ class Solution {
     private:
     bool solve(int ind1, int ind2, int ind3, string &s1, string &s2, string &s3, vector<vector<int>> &dp){
         
-       if(ind3 < 0 && (ind1 >= 0 || ind2 >= 0))return false;
+        if(ind3 < 0 && (ind1 >= 0 || ind2 >= 0))return false;
         if(ind1 < 0  && ind2 < 0 )return true;
-    if( dp[ind1 + 1][ind2 + 1] != -1)return dp[ind1 + 1][ind2 + 1];
+        if( dp[ind1 + 1][ind2 + 1] != -1)return dp[ind1 + 1][ind2 + 1];
         int take1 = 0;
         int take2 = 0;
         if(ind1 >= 0 && ind2 >= 0 && s1[ind1] == s3[ind3] && s2[ind2] == s3[ind3]){
@@ -15,7 +15,7 @@ class Solution {
             take2 = solve(ind1, ind2-1, ind3-1, s1, s2, s3, dp);
         }
         
-        return (take1 || take2);
+        return dp[ind1 + 1][ind2 + 1] = (take1 || take2);
     }
     
     
