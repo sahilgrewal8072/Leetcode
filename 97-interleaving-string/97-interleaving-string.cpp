f9cf634735c0acb80551d702be9cc2e6e40e4f30
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-07 15:51:11 +0530

    Time: 0 ms (100.00%), Space: 7.1 MB (48.95%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -1,16 +1,14 @@
 class Solution {
     private:
     bool solve(int ind1, int ind2, int ind3, string &s1, string &s2, string &s3, vector<vector<int>> &dp){
-        if(ind1 >= 0 && ind2 >= 0 && dp[ind1][ind2] != -1)return dp[ind1][ind2];
+        
        if(ind3 < 0 && (ind1 >= 0 || ind2 >= 0))return false;
         if(ind1 < 0  && ind2 < 0 )return true;
-    
-        // if(dp[ind1+1][ind2+1][ind3+1] != -1)return dp[ind1][ind2][ind3];
-        
+    if( dp[ind1 + 1][ind2 + 1] != -1)return dp[ind1 + 1][ind2 + 1];
         int take1 = 0;
         int take2 = 0;
         if(ind1 >= 0 && ind2 >= 0 && s1[ind1] == s3[ind3] && s2[ind2] == s3[ind3]){
-            return dp[ind1][ind2] = (solve(ind1-1, ind2, ind3-1, s1, s2, s3, dp) || solve(ind1, ind2-1, ind3-1, s1, s2, s3, dp));
+            return dp[ind1 + 1][ind2 + 1] = (solve(ind1-1, ind2, ind3-1, s1, s2, s3, dp) || solve(ind1, ind2-1, ind3-1, s1, s2, s3, dp));
         }else if(ind1 >= 0 && s1[ind1] == s3[ind3]){
             take1 = solve(ind1-1, ind2, ind3-1, s1, s2, s3, dp);
         }else if(ind2 >= 0 && s2[ind2] == s3[ind3]){
