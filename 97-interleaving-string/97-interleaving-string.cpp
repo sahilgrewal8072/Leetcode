b9f913c9ddca9f840ed5521e206682ebe5d11bdc
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-07 16:42:25 +0530

    Time: 5 ms (65.30%), Space: 6.4 MB (79.41%) - LeetHub

--- 97-interleaving-string/97-interleaving-string.cpp
+++ 97-interleaving-string/97-interleaving-string.cpp
@@ -24,28 +24,29 @@ public:
         int n = s1.length();
         int l = s2.length();
         int m = s3.length();
-        vector<vector<int>> dp(n+1, vector<int>(l+1, 0));
+        // vector<vector<int>> dp(n+1, vector<int>(l+1, 0));
+        vector<int> prev(l + 1, 0), curr(l + 1, 0);
+       
         if(n + l != m)return false;
         // return solve(n-1, l-1, m-1, s1, s2, s3, dp);
+        prev[0] = 1;
+        curr[0] = 1;
         for(int ind1 = 0; ind1 <=n; ind1++){
             for(int ind2 = 0; ind2 <= l; ind2++){
+                int result = 0;
                 if(ind1 == 0 && ind2 == 0){
-                    dp[ind1][ind2] = true;
+                     continue;
                 }else if(ind1 > 0 && ind2 > 0 && s1[ind1 - 1] == s3[ind1 + ind2 - 1] && s2[ind2 - 1] == s3[ind1 + ind2 - 1]){
-                    dp[ind1][ind2] = (dp[ind1-1][ind2] || dp[ind1][ind2-1]);
-                }else if(ind2 == 0){
-                    if(s1[ind1-1] == s3[ind1-1])
-                    dp[ind1][ind2] = dp[ind1-1][ind2];
-                }else if(ind1 == 0){
-                    if(s2[ind2-1] == s3[ind2 - 1])
-                    dp[ind1][ind2] = dp[ind1][ind2-1];
-                }else if(s1[ind1 - 1] != s3[ind1 + ind2 - 1] && s2[ind2 - 1] == s3[ind1 + ind2 - 1]){
-                    dp[ind1][ind2] = dp[ind1][ind2-1];
-                }else if(s1[ind1 - 1] == s3[ind1 + ind2 - 1] && s2[ind2 - 1] != s3[ind1 + ind2 - 1]){
-                     dp[ind1][ind2] = dp[ind1-1][ind2];
+                    result = (prev[ind2] || curr[ind2-1]);
+                }else if(ind1 > 0 && s1[ind1-1] == s3[ind1 + ind2 -1]){
+                    result = prev[ind2];
+                }else if(ind2 > 0 && s2[ind2-1] == s3[ind1 + ind2 - 1]){
+                    result = curr[ind2-1];
                 }
+                curr[ind2] = result;
             }
+            prev = curr;
         }
-        return dp[n][l];
+        return prev[l];
     }
 };
