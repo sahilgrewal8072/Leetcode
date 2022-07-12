dac8d843f98990cf6e7130c47e6cec27b5b994b7
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-12 19:08:20 +0530

    Time: 89 ms (64.87%), Space: 57.8 MB (8.46%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,35 +1,35 @@
 class Solution {
     private:
-    int solve(int ind, int curr_sum, int mask, int k, int target, int n, vector<int> &nums, vector<int> &dp){
+    int solve(int sum, int mask, int k, int target, vector<int> &arr, vector<int> &dp, int n){
         if(k == 0)return true;
         if(dp[mask] != -1)return dp[mask];
         int ans = 0;
         for(int i=0; i<n; i++){
             if(mask & (1 << i))continue;
-            if(curr_sum + nums[i] > target)break;
-            if(curr_sum + nums[i] == target){
+            if(sum + arr[i] > target)break;
+            if(sum + arr[i] == target){
                 mask ^= (1 << i);
-                ans = max(ans, solve(ind, 0, mask, k-1, target, n, nums, dp));
+                ans = max(ans,solve(0, mask, k-1, target, arr, dp, n));
                 mask ^= (1 << i);
             }else{
                 mask ^= (1 << i);
-                ans = max(ans, solve(i + 1, curr_sum + nums[i], mask, k, target, n, nums, dp));
+                ans = max(ans, solve(sum + arr[i], mask, k, target, arr, dp, n));
                 mask ^= (1 << i);
             }
+            
             if(ans == 1)break;
         }
-        
         return dp[mask] = ans;
     }
     
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int n = nums.size();
+        vector<int> dp((1 << 16) + 1, -1);
         sort(nums.begin(), nums.end());
         int sum = 0;
         for(auto &it : nums)sum += it;
-        if(k > n || sum%k != 0)return false;
-        vector<int> dp((1<<16) + 1, -1);
-        return solve(0, 0, 0, k-1, sum/k, n, nums, dp);
+        if(sum%k != 0 || k > n)return false;
+        return solve(0, 0, k-1, sum/k, nums, dp, n);
     }
 };
