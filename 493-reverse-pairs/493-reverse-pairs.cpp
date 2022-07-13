84f7f71b16738f1196a98a683bee84e3afd8eadc
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-13 23:17:44 +0530

    Time: 612 ms (65.39%), Space: 45.1 MB (95.51%) - LeetHub

--- 493-reverse-pairs/493-reverse-pairs.cpp
+++ 493-reverse-pairs/493-reverse-pairs.cpp
@@ -5,42 +5,13 @@ class Solution {
         int i = s;
         int mid = (s + e)/2;
         int j = mid + 1;
-        for(int low = s; low <= mid; low++){
-            while(j <= e && arr[low] > 1ll*2*arr[j]){
+        for(i = s; i <= mid; i++){
+            while(j <= e && arr[i] > 1ll*2*arr[j]){
                 j++;
             }
             count += (j - (mid + 1));
         }
-        
-        i = s;
-        j = mid + 1;
-        vector<int> temp;
-        while(i <= mid && j <= e){
-            if(arr[i] < arr[j]){
-                temp.push_back(arr[i]);
-                i++;
-            }else{
-                // count += (mid - i + 1);
-                temp.push_back(arr[j]);
-                j++;
-            }
-        }
-        
-        while(i <= mid){
-            temp.push_back(arr[i]);
-            i++;
-        }
-        
-        while(j <= e){
-            temp.push_back(arr[j]);
-            j++;
-        }
-        
-        int k = 0;
-        for(int ind = s; ind<=e; ind++){
-            arr[ind] = temp[k];
-            k++;
-        }   
+        sort(arr.begin() + s, arr.begin() + e + 1);
         return count;
     }
     
@@ -48,10 +19,10 @@ class Solution {
     int inversion_count(vector<int> &arr, int s, int e){
         if(s >= e)return 0;
         int mid = (s + e)/2;
-        int c1 = inversion_count(arr, s, mid);
-        int c2 = inversion_count(arr, mid + 1, e);
-        int cross_count = merge(arr, s, e);
-        return c1 + c2 + cross_count;
+        int count = inversion_count(arr, s, mid);
+        count += inversion_count(arr, mid + 1, e);
+        count += merge(arr, s, e);
+        return count;
     }
     
 public:
