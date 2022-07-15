fa3055342ac11fc2c4ad68d4df9d7b9dddff7851
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-15 14:36:41 +0530

    Time: 96 ms (86.73%), Space: 39.3 MB (31.61%) - LeetHub

--- 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
+++ 1310-xor-queries-of-a-subarray/1310-xor-queries-of-a-subarray.cpp
@@ -1,22 +1,63 @@
+struct segmentTree {
+	int n;
+	vector<int> st;
+
+	void init(int _n) {
+		this->n = _n;
+		st.resize(4 * n, 0);
+	}
+
+	void build(int start, int ending, int node, vector<int> &v) {
+		// leaf node base case
+		if (start == ending) {
+			st[node] = v[start];
+			return;
+		}
+
+		int mid = (start + ending) / 2;
+
+		// left subtree is (start,mid)
+		build(start, mid, 2 * node + 1, v);
+
+		// right subtree is (mid+1,ending)
+		build(mid + 1, ending, 2 * node + 2, v);
+
+		st[node] = st[node * 2 + 1] ^ st[node * 2 + 2];
+	}
+
+	int query(int start, int ending, int l, int r, int node) {
+		// non overlapping case
+		if (start > r || ending < l) {
+			return 0;
+		}
+
+		// complete overlap
+		if (start >= l && ending <= r) {
+			return st[node];
+		}
+
+		// partial case
+		int mid = (start + ending) / 2;
+
+		int q1 = query(start, mid, l, r, 2 * node + 1);
+		int q2 = query(mid + 1, ending, l, r, 2 * node + 2);
+
+		return q1 ^ q2;
+	}
+};
 class Solution {
 public:
     vector<int> xorQueries(vector<int>& arr, vector<vector<int>>& queries) {
         int n = arr.size();
-        vector<int> prefix(n, 0);
-        prefix[0] = arr[0];
-        for(int i = 1; i<n; i++){
-            prefix[i] = prefix[i-1]^arr[i];
-        }
-        
+        segmentTree tree;
+        tree.init(n);
+        tree.build(0, n-1, 0, arr);
         vector<int> ans;
         for(auto &it : queries){
             int left = it[0];
             int right = it[1];
-            int x = 0;
-            if(left - 1 >= 0){
-                x = prefix[left-1];
-            }
-            ans.push_back(prefix[right]^x);
+            int curr = tree.query(0, n-1, left, right, 0);
+            ans.push_back(curr);
         }
         return ans;
     }
