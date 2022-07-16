676f9753a3bdfb5ebb367be195d5c67bf037b8dd
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-16 12:30:30 +0530

    Time: 74 ms (11.14%), Space: 9.2 MB (54.93%) - LeetHub

--- 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
+++ 576-out-of-boundary-paths/576-out-of-boundary-paths.cpp
@@ -1,4 +1,4 @@
-int dx[] = {-1, 1, 0, 0};
+    int dx[] = {-1, 1, 0, 0};
     int dy[] = {0, 0, -1, 1};
 const int M = 1e9 + 7;
 class Solution {
@@ -18,10 +18,32 @@ class Solution {
         }
         return dp[x][y][moves] = ans%M; 
     }
+    
+    bool isOut(int x, int y, int m, int n){
+        return (x < 0 || y < 0 || x >= m || y >= n);
+    }
 public:
     int findPaths(int m, int n, int maxMove, int r, int c) {
         int count = 0;
-        vector<vector<vector<int>>> dp(m, vector<vector<int>> (n, vector<int>(maxMove + 1, -1)));
-        return solve(r, c, m, n, maxMove, dp);
+        vector<vector<vector<int>>> dp(m, vector<vector<int>> (n, vector<int>(maxMove + 1, 0)));
+        // return solve(r, c, m, n, maxMove, dp);
+        for(int moves=1; moves <= maxMove; moves++){
+            for(int x = 0; x < m; x++){
+                for(int y = 0; y < n; y++){
+                    for(int dir = 0; dir < 4; dir++){
+                        int nx = x + dx[dir];
+                        int ny = y + dy[dir];
+                        if(isOut(nx, ny, m, n)){
+                            dp[x][y][moves] += 1;
+                            dp[x][y][moves] %= M;
+                        }else{
+                            dp[x][y][moves] += (dp[nx][ny][moves-1])%M;
+                            dp[x][y][moves] %= M;
+                        }
+                    }
+                }
+            }
+        }
+        return dp[r][c][maxMove];
     }
 };
