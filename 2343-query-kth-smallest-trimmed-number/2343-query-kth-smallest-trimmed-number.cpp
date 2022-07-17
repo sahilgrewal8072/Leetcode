d0389ea88f021b6fe037c4720a3e5bd9c83bd92f
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-07-17 12:44:19 +0530

    Time: 2371 ms (11.11%), Space: 407.8 MB (11.11%) - LeetHub

--- 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
+++ 2343-query-kth-smallest-trimmed-number/2343-query-kth-smallest-trimmed-number.cpp
@@ -3,23 +3,15 @@ class Solution {
     int findKthsmallest(vector<string>& nums, int k, int n) {
         priority_queue<pair<string, int>> pq;
         for(int i=0; i<n; i++){
-            // int x = stoi(nums[i]);
-            // cout<<x<<" ";
              pq.push({nums[i], i});
             if(pq.size() > k)pq.pop();        
         }
-        // cout<<endl;
         return pq.top().second;
     }
 
     
 public:
-    static bool cmp(vector<int> &a,  vector<int> &b){
-        return a[1] > b[1];
-    }
-    
     vector<int> smallestTrimmedNumbers(vector<string>& nums, vector<vector<int>>& queries) {
-        // sort(queries.begin(), queries.end(), cmp);
         int n = queries.size();
         int m = nums.size();
         int len = nums[0].length();
@@ -27,13 +19,11 @@ public:
         vector<string> nums1(m);
         for(int i=0; i<n; i++){
             for(int j = 0; j < m; j++){
-                // if(len - queries[i][1] <= 0)continue;
                 nums1[j] = nums[j].substr(len - queries[i][1], len);
             }
-            // for(auto it : nums1)cout<<it<<" ";
-            // cout<<endl;
             ans[i] = findKthsmallest(nums1, queries[i][0], m);
         }
+        
         return ans;
     }
 };
