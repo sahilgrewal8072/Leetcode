309c2863f719e3c7e362105b501db1d54396f484
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-08-12 00:39:16 +0530

    Time: 0 ms (100.00%), Space: 12 MB (44.33%) - LeetHub

--- 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
+++ 199-binary-tree-right-side-view/199-binary-tree-right-side-view.cpp
@@ -11,13 +11,14 @@
  */
 class Solution {
     private:
-    void solve(TreeNode* root, int level, vector<int> &ans){
-        if(!root)return;
-        if(ans.size() < level)ans.push_back(root->val);
-        solve(root->right, level + 1, ans);
-        solve(root->left, level + 1, ans);
+void solve(TreeNode* root, int lvl, vector<int> &ans){
+    if(!root)return;
+    if(lvl > ans.size()){
+        ans.push_back(root->val);
     }
-    
+    solve(root->right, lvl + 1, ans);
+    solve(root->left, lvl + 1, ans);
+}
 public:
     vector<int> rightSideView(TreeNode* root) {
         vector<int> ans;
