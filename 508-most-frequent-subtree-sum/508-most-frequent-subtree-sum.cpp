d94060536b022212130619ea3e6b8dfeb0fd6155
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-08-22 22:52:04 +0530

    Time: 37 ms (30.81%), Space: 24.6 MB (48.00%) - LeetHub

--- 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
+++ 508-most-frequent-subtree-sum/508-most-frequent-subtree-sum.cpp
@@ -11,26 +11,27 @@
  */
 class Solution {
     private:
-    int solve(TreeNode* root, unordered_map<int, int> &mp, int &mx_sum){
+    int solve(TreeNode* root, unordered_map<int, int> &mp, int &mx_freq){
         if(!root)return 0;
         
-        int left = solve(root->left, mp, mx_sum);
-        int right = solve(root->right,  mp, mx_sum);
+        int left = solve(root->left, mp, mx_freq);
+        int right = solve(root->right,  mp, mx_freq);
         int sum = root->val + left + right;
         mp[sum]++;
-        if(mp[sum] > mx_sum){
-            mx_sum = mp[sum];
-        }
+        // if(mp[sum] > mx_sum){
+        //     mx_sum = mp[sum];
+        // }
+        mx_freq = max(mx_freq, mp[sum]);
         return sum;
     }
 public:
     vector<int> findFrequentTreeSum(TreeNode* root) {
         unordered_map<int, int> mp;
-        int mx_sum = 0;
-        solve(root, mp, mx_sum);
+        int mx_freq = 0;
+        solve(root, mp, mx_freq);
         vector<int> ans;
         for(auto &it : mp){
-            if(it.second == mx_sum){
+            if(it.second == mx_freq){
                 ans.push_back(it.first);
             }
         }
