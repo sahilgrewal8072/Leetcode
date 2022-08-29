29bae83bf296d1193d9a79f3162e8f232a8f9bc0
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-08-29 20:08:28 +0530

    Time: 120 ms (82.20%), Space: 44 MB (20.37%) - LeetHub

--- 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
+++ 907-sum-of-subarray-minimums/907-sum-of-subarray-minimums.cpp
@@ -1,25 +1,35 @@
 class Solution {
 public:
-    long long int mod = 1e9 + 7;
+    int mod = 1e9 + 7;
     int sumSubarrayMins(vector<int>& arr) {
-        stack<int> st;
+        // ple = previous less element 
+        // nle = next less element
+        stack<pair<int, int>> ple, nle;
         int n = arr.size();
-        int sum = 0;
-        for(int i=0; i<=n; i++){
-            while(!st.empty() && (i==n || arr[st.top()] > arr[i])){
-                int j = st.top();
-                st.pop();
-               long long int right = i - j;
-               long long int prevTop = -1;
-                if(!st.empty()){
-                    prevTop = st.top();
-                }
-                int left = j - prevTop;
-                sum += (long long)(arr[j] * (right*left)%mod)%mod;
-                sum %= mod;
-            }
-            st.push(i);
+        vector<int> left(n);
+        vector<int> right(n);
+        for(int i=0; i<n; i++){
+            right[i] = n - i;
         }
-        return sum%mod;
+        int sum = 0;
+        for(int i=0; i<n; i++){
+            while(!ple.empty() && arr[ple.top().second] > arr[i])ple.pop();
+            left[i] = (ple.empty()) ? i+1 : i - ple.top().second;
+            ple.push({arr[i], i});
+            
+            
+            while(!nle.empty() && arr[nle.top().second] > arr[i]){
+                int ind = nle.top().second;
+                nle.pop();
+                right[ind] = i - ind;
+            }
+            nle.push({arr[i], i});
+        }
+        
+        for(int i=0; i<n; i++){
+            sum += (arr[i]*1ll*(left[i]*right[i])%mod)%mod;
+            sum %= mod;
+        }
+        return sum;
     }
 };
