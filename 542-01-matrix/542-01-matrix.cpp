e360fbffc7b70b48bfd8e7ae7270095bdbe5d1a2
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-08-30 19:29:57 +0530

    Time: 113 ms (62.16%), Space: 30.3 MB (54.56%) - LeetHub

--- 542-01-matrix/542-01-matrix.cpp
+++ 542-01-matrix/542-01-matrix.cpp
@@ -6,7 +6,7 @@ public:
         queue<pair<int, int>> q;
         int dx[] = {-1, 1, 0, 0};
         int dy[] = {0, 0, 1, -1};
-        vector<vector<int>> dist(m, vector<int>(n, -1));
+        vector<vector<int>> dist(m, vector<int>(n, INT_MAX));
         for(int i=0; i<m; i++){
             for(int j=0; j<n; j++){
                 if(mat[i][j] == 0){
@@ -25,7 +25,7 @@ public:
             for(int i=0; i<4; i++){
                 int nx = x + dx[i];
                 int ny = y + dy[i];
-                if(nx >= 0 && ny >= 0 && nx < m && ny < n && dist[nx][ny] == -1){
+                if(nx >= 0 && ny >= 0 && nx < m && ny < n && dist[nx][ny] > dist[x][y] + 1){
                     q.push({nx, ny});
                     dist[nx][ny] = dist[x][y] + 1;
                 }
