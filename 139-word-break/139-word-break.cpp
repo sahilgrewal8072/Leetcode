83cfc5ba7d1b6555af013b158a5c2dae03f453dc
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-09 01:40:35 +0530

    Time: 10 ms (72.23%), Space: 8 MB (81.60%) - LeetHub

--- 139-word-break/139-word-break.cpp
+++ 139-word-break/139-word-break.cpp
@@ -1,41 +1,27 @@
 class Solution {
-public:
-    
-    bool solve(int ind, string &s, unordered_set<string> &st, int n, vector<int> &dp){
-        if(ind >= n)return true;
-        if(dp[ind] != -1)return dp[ind];
-        string sub = "";
-        for(int i = ind; i < n; i++){
-            if(st.count(sub += s[i]) > 0 && solve(i + 1, s, st, n, dp)){
-                return dp[ind] = true;
-            }
+    private:
+        bool solve(int ind, unordered_set<string> &st, string &s, int n, vector<int> &dp){
+            if(ind >= n)return true;
+            if(dp[ind] != -1)return dp[ind];
+            string curr = "";
+            for(int i=ind; i<n; i++){
+                curr += s[i];
+                if(st.count(curr) > 0 && solve(i + 1, st, s, n, dp)){
+                   return dp[ind] = true;
+                }
+            }       
+           return dp[ind] = false;
         }
-        return dp[ind] = false;
-    }
     
+public:
     bool wordBreak(string s, vector<string>& wordDict) {
-        int n = s.length();
-        vector<int> dp(n+1, 0);
         unordered_set<string> st;
+        
         for(auto it : wordDict){
             st.insert(it);
         }
-        
-        dp[n] = 1;
-        for(int ind = n-1; ind >= 0; ind--){
-            string sub = "";
-            bool flag = false;
-            for(int i = ind; i < n; i++){
-                if(st.count(sub += s[i]) > 0 && dp[i + 1] == 1){
-                    dp[ind] = 1;
-                    flag = true;
-                }
-            }
-            if(!flag)
-            dp[ind] = 0;
-        }
-        
-        return dp[0];
-        // return solve(0, s, st, n, dp);
+        int n = s.length();
+        vector<int> dp(n, -1);
+        return solve(0, st, s, n, dp);
     }
 };
