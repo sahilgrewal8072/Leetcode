6cc35f1670d453c611d399c03629320f1b5937de
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-09 00:05:48 +0530

    Time: 25 ms (28.23%), Space: 7.3 MB (52.83%) - LeetHub

--- 51-n-queens/51-n-queens.cpp
+++ 51-n-queens/51-n-queens.cpp
@@ -1,58 +1,61 @@
 class Solution {
     private:
-        bool isPossible(vector<vector<int>> &board, int &x, int &y, int &n){
-            int i=x, j=y;
-            for(; i>=0 && j>=0; i--, j--){
-                if(board[i][j] == 1)return false;
-            }
-            
-            i = x, j = y;
-            for(; i>=0 && j < n; i--, j++){
-                if(board[i][j] == 1)return false;
-            }
-            
-            j = y;
-            i = x;
-            for(; i>=0; i--){
-                if(board[i][y] == 1)return false;
-            }
-            return true;
+    bool isPossible(int x, int y, vector<vector<int>> &board, int n){
+        // check diagonal
+        int _x = x;
+        int _y = y;
+        for(; _x >= 0 && _y >= 0; _x--, _y--){
+            if(board[_x][_y] == 1)return false;
         }
+        
+        // check for anti-diagonal
+        _x = x;
+        _y = y;
+        for(; _x >= 0 && _y < n; _x--, _y++){
+           if(board[_x][_y] == 1)return false;
+        }
+        
+        _x = x;
+        _y = y;
+        for(; _x >= 0 ; _x--){
+            if(board[_x][y] == 1)return false;
+        }
+        
+        return true;
+    }
     
-    private:
-    void solve(int ind, string &curr, vector<string> &stocks, vector<vector<int>> &board, int &n, vector<vector<string>> &ans){
+    private: 
+    void solve(int ind, string &curr, vector<vector<int>> &board, int n, vector<vector<string>> &ans, vector<string> &moves){
         if(ind == n){
-            ans.push_back(stocks);
+            ans.push_back(moves);
             return;
         }
         
         string x = curr;
         for(int j=0; j<n; j++){
-            if(isPossible(board, ind, j, n)){
-                board[ind][j] = 1;
+            if(isPossible(ind, j, board, n)){
                 curr[j] = 'Q';
-                stocks.push_back(curr);
-                solve(ind + 1, x, stocks, board, n, ans);
-                curr[j] = '.';
+                board[ind][j] = 1;
+                moves.push_back(curr);
+                solve(ind + 1, x, board, n, ans, moves);
                 board[ind][j] = 0;
-                stocks.pop_back();
+                curr[j] = '.';
+                moves.pop_back();
             }
         }
-        return;
     }
-    
     
 public:
     vector<vector<string>> solveNQueens(int n) {
-        string curr;
+        vector<vector<string>> ans;
+        vector<vector<int>> board(n, vector<int>(n, 0));
+        vector<string> moves;
+        string curr = "";
         for(int i=0; i<n; i++){
-            curr += '.';
+            curr += ".";
         }
         
-        vector<string> stocks;
-        vector<vector<int>> board(n, vector<int>(n, 0));
-        vector<vector<string>> ans;
-        solve(0, curr, stocks, board, n, ans);
+        solve(0, curr, board, n, ans, moves);
         return ans;
     }
 };
