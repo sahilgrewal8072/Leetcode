c76a1d012a8aaf94b23281a3d9345c1ceb64211d
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-09 17:46:36 +0530

    Time: 64 ms (26.18%), Space: 6.5 MB (51.22%) - LeetHub

---
+++ 37-sudoku-solver/37-sudoku-solver.cpp
@@ -0,0 +1,45 @@
+class Solution {
+    private:
+    bool solve(vector<vector<char>> &board){
+        
+        for(int i=0; i<9; i++){
+            for(int j=0; j<9; j++){
+                if(board[i][j] == '.'){
+                    for(char c = '1'; c <= '9'; c++){
+                        if(isValid(board, i, j, c)){
+                            board[i][j] = c;
+                            if(solve(board)){
+                                return true;
+                            }else{
+                                board[i][j] = '.';
+                            }   
+                    } 
+                }
+                    return false;
+            }
+        }
+    }
+        return true;
+    }
+    
+    private:
+    
+    bool isValid(vector<vector<char>> &board, int row, int col, char c){
+        for(int i=0; i<9; i++){
+            // check row
+            if(board[row][i] == c)return false;
+            
+            // check col
+            if(board[i][col] == c)return false;
+            
+            // check submatrix ps. must revise
+            if(board[3*(row/3) + i/3][3*(col/3) + i%3] == c)return false;
+        }
+        return true;
+    }
+    
+public:
+    void solveSudoku(vector<vector<char>>& board) {
+        solve(board);
+    }
+};
