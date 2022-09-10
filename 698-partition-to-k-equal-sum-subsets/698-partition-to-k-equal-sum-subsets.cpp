b1e2927022174cd761166a57889903338220a90c
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-10 16:25:16 +0530

    Time: 117 ms (69.04%), Space: 57.7 MB (8.62%) - LeetHub

--- 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
+++ 698-partition-to-k-equal-sum-subsets/698-partition-to-k-equal-sum-subsets.cpp
@@ -1,35 +1,42 @@
 class Solution {
     private:
-    int solve(int sum, int mask, int k, int target, vector<int> &arr, vector<int> &dp, int n){
-        if(k == 0)return true;
-        if(dp[mask] != -1)return dp[mask];
+    int solve(int curr_sum, int mask, vector<int> &nums, int limit, vector<int> &dp, int &n, int target){
         int ans = 0;
+        if(limit == 0)return true;
+        if(dp[mask] != -1)return dp[mask];
         for(int i=0; i<n; i++){
+            if(curr_sum + nums[i] > target)break;
             if(mask & (1 << i))continue;
-            if(sum + arr[i] > target)break;
-            if(sum + arr[i] == target){
+            if(curr_sum + nums[i] == target){
                 mask ^= (1 << i);
-                ans = max(ans,solve(0, mask, k-1, target, arr, dp, n));
+                ans = max(ans, solve(0, mask, nums, limit-1, dp, n, target));
                 mask ^= (1 << i);
             }else{
                 mask ^= (1 << i);
-                ans = max(ans, solve(sum + arr[i], mask, k, target, arr, dp, n));
+                ans = max(ans, solve(curr_sum + nums[i], mask, nums, limit, dp, n, target));
                 mask ^= (1 << i);
             }
             
             if(ans == 1)break;
         }
-        return dp[mask] = ans;
+        
+        return dp[mask] =  ans;
     }
     
 public:
     bool canPartitionKSubsets(vector<int>& nums, int k) {
         int n = nums.size();
-        vector<int> dp((1 << 16) + 1, -1);
-        sort(nums.begin(), nums.end());
         int sum = 0;
-        for(auto &it : nums)sum += it;
-        if(sum%k != 0 || k > n)return false;
-        return solve(0, 0, k-1, sum/k, nums, dp, n);
+        sort(nums.begin(), nums.end());
+        vector<int> dp((1 << 16) + 1, -1);
+        for(auto &it : nums){
+            sum += it;
+        }
+        if(k > n || sum%k != 0){
+            return false;
+        }
+        if(k == 1)return true;
+        return solve(0, 0, nums, k, dp, n, sum/k);
     }
+    
 };
