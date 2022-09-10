0347741aa7de82686d58bd5a733ffcd8a26bb36e
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-10 22:40:20 +0530

    Time: 47 ms (48.98%), Space: 39.9 MB (20.05%) - LeetHub

--- 494-target-sum/494-target-sum.cpp
+++ 494-target-sum/494-target-sum.cpp
@@ -2,19 +2,22 @@ class Solution {
 public:
     
     
-    int solve(int n, vector<int> &nums, int target){
+    int solve(int n, vector<int> &nums, int target, int sum, vector<vector<int>> &dp){
         if(n == 0){
-            if(target == 0){
+            if(target == sum){
                 return 1;
             }
             return 0;
         }
         
-        return (solve(n-1, nums, target - nums[n-1]) + solve(n-1, nums, target + nums[n-1]));
+        // if target becomes -ve + 100 will help to shift the index
+        if(dp[n][sum + 1000] != -1)return dp[n][sum + 1000];
+        return dp[n][sum + 1000] = (solve(n-1, nums, target, sum - nums[n-1], dp) + solve(n-1, nums, target, sum + nums[n-1], dp));
     }
     
     int findTargetSumWays(vector<int>& nums, int target) {
         int n = nums.size();
-       return solve(n, nums, target);
+        vector<vector<int>> dp(n+1, vector<int>(2005, -1));
+       return solve(n, nums, target, 0, dp);
     }
 };
