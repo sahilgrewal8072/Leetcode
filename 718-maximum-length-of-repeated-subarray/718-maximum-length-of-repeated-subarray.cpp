9c35e4c0f051005a709dbd6dba49490e0cdda773
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-11 15:38:48 +0530

    Time: 2025 ms (5.01%), Space: 109 MB (52.85%) - LeetHub

---
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -0,0 +1,24 @@
+class Solution {
+    private:
+    int overall = 0;
+    int solve(int ind1, int ind2, vector<int> &nums1, vector<int> &nums2, vector<vector<int>> &dp){
+        if(ind1 == 0 || ind2 == 0)return 0;
+        int ans = 0;
+        if(dp[ind1][ind2] != -1)return dp[ind1][ind2];
+        if(nums1[ind1-1] == nums2[ind2-1]){
+            ans = max(1 + solve(ind1-1, ind2-1, nums1, nums2, dp), 0);
+        }
+        overall = max(overall, ans);
+        solve(ind1-1, ind2, nums1, nums2, dp);
+        solve(ind1, ind2-1, nums1, nums2, dp);
+        return dp[ind1][ind2] = ans;
+    }
+public:
+    int findLength(vector<int>& nums1, vector<int>& nums2) {
+        int m = nums1.size();
+        int n = nums2.size();
+        vector<vector<int>> dp(m+1, vector<int>(n+1, -1));
+        solve(m, n, nums1, nums2, dp);
+        return overall;
+    }
+};
