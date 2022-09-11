42c440dafa6ed583612e8ee6d57c55f97b3374d1
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-11 15:44:19 +0530

    Time: 335 ms (77.84%), Space: 206 MB (14.24%) - LeetHub

--- 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
+++ 718-maximum-length-of-repeated-subarray/718-maximum-length-of-repeated-subarray.cpp
@@ -21,14 +21,17 @@ public:
         // solve(m, n, nums1, nums2, dp);
         // return overall;
         int ans = 0;
+        vector<int> prev(n + 1, 0);
+       
         for(int ind1 = 1; ind1 <= m; ind1++){
+             vector<int> curr(n + 1, 0);
             for(int ind2 = 1; ind2 <= n; ind2++){
                 if(nums1[ind1-1] == nums2[ind2-1]){
-                    dp[ind1][ind2] = max(1 + dp[ind1-1][ind2-1], 0);
+                    curr[ind2] = max(1 + prev[ind2-1], 0);
                 }
-                ans = max(ans, dp[ind1][ind2]);
+                ans = max(ans, curr[ind2]);
             }
-            
+            prev = curr;
         }
         return ans;
     }
