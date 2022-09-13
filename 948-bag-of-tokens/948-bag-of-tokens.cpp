030ecc433b02ddab888417765a0387763f571674
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-13 15:05:08 +0530

    Time: 17 ms (15.74%), Space: 10.6 MB (67.18%) - LeetHub

--- 948-bag-of-tokens/948-bag-of-tokens.cpp
+++ 948-bag-of-tokens/948-bag-of-tokens.cpp
@@ -1,41 +1,29 @@
 class Solution {
 public:
     int bagOfTokensScore(vector<int>& arr, int power) {
-        int n = arr.size(); // extract size of array
-        
-        int score = 0; // intially we have score zero
-        
-        sort(arr.begin(), arr.end()); // sort the array
-        
-        int i = 0, j = n - 1; // put two pointers on first and last
-        
-        // equal sign is not here, because for a single element,
-        // we can't use it as to increase score and also use 
-        // it as to increase power
-        while(i < j) 
-        {
-            if(power >= arr[i]) // if we have available power to increase score, use it
-            {
-                power -= arr[i++];
-                score++;
-            }   
-			                                                // for power to be gain, we should have at least one score
-            else if(score >= 1 && power + arr[j] >= arr[i]) // and we have to make sure by gaining this power we are able to earn score
-                                                           //  else it makes no point of spending score
-            {
-                power += arr[j--];
-                score--;
+        int n = arr.size();
+        sort(arr.begin(), arr.end());
+    
+        int score_gain = 0;
+        int left = 0;
+        int right = n-1;
+        int mx = 0;
+        while(left <= right){
+            while(left < n && power - arr[left] >= 0 ){
+                    score_gain++;
+                    power -= arr[left];
+                    left++;
+                    mx = max(mx, score_gain);
             }
-            else break; // because we already know that array is sorted,
-			//so further values wil be definately in increasing order
+            
+            if(right >= 0 && score_gain > 0){
+                score_gain--;
+                power += arr[right];
+                right--;
+            }else if(score_gain == 0){
+                break;
+            }
         }
-        
-        while(i < j + 1 && power >= arr[i]) // when gap remains, then for that
-        {
-            power -= arr[i++];
-            score++;
-        }
-        
-        return score; // finally return score
+        return mx;
     }
 };
