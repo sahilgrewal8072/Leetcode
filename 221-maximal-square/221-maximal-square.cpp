5dd102a677f807abe458a5f8a6e103ae4b70b0ef
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-15 21:30:52 +0530

    Time: 189 ms (17.34%), Space: 18.7 MB (29.44%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -20,19 +20,20 @@ public:
         int m = matrix.size();
         int n = matrix[0].size();
         int mx = 0;
-        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        vector<int> nxt(n + 1, 0), curr(n + 1, 0);
         // solve(0, 0, matrix, m, n, mx, dp);
         for(int i = m - 1; i>= 0; i--){
             for(int j = n-1 ; j >= 0 ; j--){
-                int right = dp[i][j + 1];
-                int down = dp[i+1][j];
-                int diagonal = dp[i + 1][j + 1];
+                int right = curr[j + 1];
+                int down = nxt[j];
+                int diagonal = nxt[j + 1];
                 if(matrix[i][j] == '1'){
-                    dp[i][j] = 1 + min({right, down, diagonal});
-                        mx = max(mx, dp[i][j]);
+                    curr[j] = 1 + min({right, down, diagonal});
+                        mx = max(mx, curr[j]);
                 }else
-                 dp[i][j] = 0;
+                 curr[j] = 0;
             }
+            nxt = curr;
         }
         return mx*mx;
     }
