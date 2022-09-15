aba6b5e87c49d93ddb6f5614f6c5f0e98fdc0aec
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2022-09-15 21:25:57 +0530

    Time: 230 ms (11.63%), Space: 21 MB (8.31%) - LeetHub

--- 221-maximal-square/221-maximal-square.cpp
+++ 221-maximal-square/221-maximal-square.cpp
@@ -1,28 +1,39 @@
 class Solution {
+    private:
+    int solve(int i, int j, vector<vector<char>>& matrix, int &m, int &n, int &mx, vector<vector<int>> &dp){
+        if(i >= m || j >= n){
+            return 0;
+        }
+        
+        int right = solve(i, j + 1, matrix, m, n, mx, dp);
+        int down = solve(i+1, j, matrix, m, n, mx, dp);
+        int diagonal = solve(i + 1, j + 1, matrix, m, n, mx, dp);
+        if(matrix[i][j] == '1'){
+            dp[i][j] = 1 + min({right, down, diagonal});
+            mx = max(mx, dp[i][j]);
+            return dp[i][j];
+        }
+        return dp[i][j] = 0;
+    }
 public:
     int maximalSquare(vector<vector<char>>& matrix) {
-        int n = matrix.size();
-        int m = matrix[0].size();
+        int m = matrix.size();
+        int n = matrix[0].size();
         int mx = 0;
-        bool flag = false;
-        vector<vector<int>> dp(n, vector<int> (m, 0));
-        for(int i=0; i<n; i++){
-            for(int j=0; j<m; j++){
-                if(matrix[i][j] == '1')flag = true;
-                if(i == 0 || j == 0){
-                    dp[i][j] = matrix[i][j] - '0';
-                    continue;
-                }
+        vector<vector<int>> dp(m + 1, vector<int> (n + 1, 0));
+        // solve(0, 0, matrix, m, n, mx, dp);
+        for(int i = m - 1; i>= 0; i--){
+            for(int j = n-1 ; j >= 0 ; j--){
+                int right = dp[i][j + 1];
+                int down = dp[i+1][j];
+                int diagonal = dp[i + 1][j + 1];
                 if(matrix[i][j] == '1'){
-                    dp[i][j] = 1 + min({dp[i-1][j-1] , dp[i-1][j] , dp[i][j-1] });
-                    
-                }else{
-                    dp[i][j] = 0;
-                }
-                mx = max(mx, dp[i][j]);
+                    dp[i][j] = 1 + min({right, down, diagonal});
+                        mx = max(mx, dp[i][j]);
+                }else
+                 dp[i][j] = 0;
             }
         }
-        if(mx == 0 && flag)return 1;
         return mx*mx;
     }
 };
