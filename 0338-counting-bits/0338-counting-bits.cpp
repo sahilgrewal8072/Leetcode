acc2f0c9b772df7f4aa4ae62aa95664c234a9255
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2025-05-18 14:14:39 -0500

    Time: 2 ms (33.53%), Space: 10.7 MB (36.99%) - LeetHub

--- 0338-counting-bits/0338-counting-bits.cpp
+++ 0338-counting-bits/0338-counting-bits.cpp
@@ -1,15 +1,10 @@
 class Solution {
 public:
     vector<int> countBits(int n) {
-        vector<int> ans(n+1);
-        ans[0] = 0;
-        int i = 0;
-        for(i=0; i<=n; i++){
-            if(!(i&1)){
-                ans[i] = ans[i/2];
-            }else{
-                ans[i] = 1 + ans[i/2];
-            }
+        vector<int> ans(n+1, 0);
+        for(int i = 0; i <= n; i++){
+            // ans[i] = ans[i/2] + i%2;
+            ans[i] = ans[i >> 1] + (i&1);
         }
         return ans;
     }
