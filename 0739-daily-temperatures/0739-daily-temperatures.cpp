68051219a83342fb7089180f941dcb4919dab9e9
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2025-05-21 17:50:46 -0500

    Time: N/A (0%), Space: N/A (0%) - LeetHub

---
+++ 0739-daily-temperatures/0739-daily-temperatures.cpp
@@ -0,0 +1,18 @@
+class Solution {
+public:
+    vector<int> dailyTemperatures(vector<int>& nums) {
+        int n = nums.size();
+        stack<int> st;
+        vector<int> ans(n, 0);
+        for(int i = n-1; i >= 0; i--){
+            while(!st.empty() && nums[i] > nums[st.top()]){
+                st.pop();
+            }
+
+            int nxt_warmer_day = st.size() > 0 ? st.top() - i : 0;
+            ans[i] = nxt_warmer_day;
+            st.push(i);
+        }
+        return ans;
+    }
+};
