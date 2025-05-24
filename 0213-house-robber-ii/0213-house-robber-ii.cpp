46e67d04774aaa0eea282584ccd12584b8cf8b20
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2025-05-24 11:35:03 -0500

    Time: 0 ms (100%), Space: 11 MB (21.18%) - LeetHub

---
+++ 0213-house-robber-ii/0213-house-robber-ii.cpp
@@ -0,0 +1,40 @@
+class Solution {
+    private:
+  int solve(int ind, int n, vector<int> &nums, vector<int> &dp){
+        if(ind >= n){
+            return 0;
+        }
+
+        if(dp[ind] != -1)return dp[ind];
+
+        int take = INT_MIN;
+        int notake = INT_MIN;
+
+        take  = nums[ind] + solve(ind + 2, n, nums, dp);
+        notake = 0 + solve(ind + 1, n, nums, dp);
+        return dp[ind] = max(take, notake);
+    }
+
+    int solve_itr(int start, int end, vector<int> &nums){
+        vector<int> dp(end  + 3, 0);
+        dp[end] = 0;
+        for(int ind = end; ind>=start; ind--){
+             int take = INT_MIN;
+            int notake = INT_MIN;
+
+            take  = nums[ind] + dp[ind + 2];
+            notake = 0 + dp[ind + 1];
+            dp[ind] = max(take, notake);
+        }   
+        return dp[start];
+    }
+public:
+    int rob(vector<int>& nums) {
+        int n = nums.size();
+        if(n == 1)return nums[0];
+        vector<int> dp(n, -1);
+        vector<int> dp1(n, -1);
+        // return max(solve(0, n-, nums, dp), solve(1, n, nums, dp1));
+        return max(solve_itr(0, n-2, nums), solve_itr(1, n-1, nums));
+    }
+};
