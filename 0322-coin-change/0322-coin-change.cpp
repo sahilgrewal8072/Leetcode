b7f30a14fb9033d3ee689e3d4b1126a95cf6ca78
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2025-05-25 15:59:56 -0500

    Time: 51 ms (25.06%), Space: 48.9 MB (38.09%) - LeetHub

---
+++ 0322-coin-change/0322-coin-change.cpp
@@ -0,0 +1,55 @@
+class Solution {
+    private:
+    int solve(int ind, int amount, vector<int> &coins, vector<vector<int>> &dp){
+        if(ind == 0){
+            if(amount == 0)return 0;
+            if(amount >= coins[ind]){
+                if(amount%coins[ind] == 0){
+                    return amount/coins[ind];
+                }
+            }
+            return 1e8;
+        }
+        if(dp[ind][amount] != -1)return dp[ind][amount];
+        int take = 1e8;
+        int notake = 1e8;
+
+        if(coins[ind] <= amount){
+            take = 1 + solve(ind, amount - coins[ind], coins, dp);
+        }
+        notake = 0 + solve(ind - 1, amount, coins, dp);
+        return dp[ind][amount] = min(take, notake);
+    }
+public:
+    int coinChange(vector<int>& coins, int final_amount) {
+        int n = coins.size();
+        vector<vector<int>> dp(n, vector<int>(final_amount + 1, 0));
+        // int ans =  solve(n-1, amount, coins, dp);
+        
+        for(int ind = 0; ind < n; ind++){
+            for(int amount = 1; amount <= final_amount; amount++){
+                if(ind == 0){ 
+                    dp[ind][amount] = 1e8;
+                if(amount >= coins[ind]){
+                    if(amount%coins[ind] == 0){
+                        dp[ind][amount] = amount/coins[ind];
+                    }
+                } 
+                continue;
+            }
+                
+                int take = 1e8;
+                int notake = 1e8;
+
+                if(coins[ind] <= amount){
+                    take = 1 + dp[ind][amount - coins[ind]];
+                }
+                notake = 0 + dp[ind - 1][amount];
+                 dp[ind][amount] = min(take, notake);
+            }
+        }
+        if(dp[n-1][final_amount] >= 1e8)return -1;
+        cout<<dp[n-1][final_amount]<<endl;
+        return dp[n-1][final_amount];
+    }
+};
