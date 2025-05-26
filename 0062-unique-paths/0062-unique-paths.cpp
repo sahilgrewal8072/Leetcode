0126345dfc36c4481dcefc533ee321bb738d9e26
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2025-05-26 16:55:25 -0500

    Time: 1 ms (24.67%), Space: 9.2 MB (67.96%) - LeetHub

---
+++ 0062-unique-paths/0062-unique-paths.cpp
@@ -0,0 +1,33 @@
+class Solution {
+    private:
+    int solve(int x, int y, vector<vector<int>> &dp, int m, int n){
+        if(x == m-1 && y == n-1){
+            return 1;
+        }
+        if(x >= m || y >= n)return 0;
+        cout<<x<<" "<<y<<endl;
+        if(dp[x][y] != -1)return dp[x][y];
+        int down = solve(x, y + 1, dp, m, n);
+        int right = solve(x + 1, y, dp, m, n);
+        
+        return dp[x][y] = down + right;
+    }
+public:
+    int uniquePaths(int m, int n) {
+        vector<vector<int>> dp(m, vector<int> (n, 0));
+        // return solve(0, 0, dp, m, n);
+        for(int x = m-1; x >= 0; x--){
+            for(int y = n-1; y >= 0; y--){
+                if(x == m-1 && y == n-1){
+                    dp[x][y] = 1;
+                }else{
+                    // if(x >= m || y >= n)continue;
+                    int down = y + 1 < n ?dp[x][y + 1] : 0;
+                    int right = x+1 < m ?dp[x+1][y] : 0;
+                    dp[x][y] = down + right;
+                }
+            }
+        }
+        return dp[0][0];
+    }
+};
