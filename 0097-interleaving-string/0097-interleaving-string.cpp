5e8bda115ea755db13e6cfe90cdb0d83445d7d24
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2025-05-28 17:50:47 -0500

    Time: 5 ms (46.31%), Space: 9.9 MB (34.98%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -1,52 +1,28 @@
 class Solution {
-    private:
-    bool solve(int ind1, int ind2, int ind3, string &s1, string &s2, string &s3, vector<vector<int>> &dp){
-        
-        if(ind3 < 0 && (ind1 >= 0 || ind2 >= 0))return false;
-        if(ind1 < 0  && ind2 < 0 )return true;
-        if( dp[ind1 + 1][ind2 + 1] != -1)return dp[ind1 + 1][ind2 + 1];
+    bool solve(int n1, int n2, int n3, string &s1, string &s2, string &s3, vector<vector<int>> &dp){
+        if(n3 == 0 && (n1 == 0 && n2 == 0))return true;
+        if(n3 > 0 && (n1 == 0 && n2 == 0))return false;
+        if(n3 == 0 && (n1 > 0 || n2 > 0))return false;
+        if(dp[n1][n2] != -1)return dp[n1][n2];
         int take1 = 0;
         int take2 = 0;
-        if(ind1 >= 0 && ind2 >= 0 && s1[ind1] == s3[ind3] && s2[ind2] == s3[ind3]){
-            return dp[ind1 + 1][ind2 + 1] = (solve(ind1-1, ind2, ind3-1, s1, s2, s3, dp) || solve(ind1, ind2-1, ind3-1, s1, s2, s3, dp));
-        }else if(ind1 >= 0 && s1[ind1] == s3[ind3]){
-            take1 = solve(ind1-1, ind2, ind3-1, s1, s2, s3, dp);
-        }else if(ind2 >= 0 && s2[ind2] == s3[ind3]){
-            take2 = solve(ind1, ind2-1, ind3-1, s1, s2, s3, dp);
+        if(n1 > 0 && n2 > 0 && n3 > 0 && (s1[n1 - 1] == s3[n3 - 1] && s2[n2-1] == s3[n3-1])){
+            return dp[n1][n2] = solve(n1 - 1, n2, n3-1, s1, s2, s3, dp) || solve(n1, n2-1, n3-1, s1, s2, s3, dp);
+        }else if(n1 > 0 && n3 > 0 && (s1[n1-1] == s3[n3-1])){
+            take1 = solve(n1-1, n2, n3-1, s1, s2, s3, dp);
+        }else if(n2 > 0 && n3 > 0 && (s2[n2-1] == s3[n3-1])){
+            take2 = solve(n1, n2-1, n3-1, s1, s2, s3, dp);
         }
-        
-        return dp[ind1 + 1][ind2 + 1] = (take1 || take2);
+        return dp[n1][n2] = take1 || take2;
     }
-    
-    
+
 public:
     bool isInterleave(string s1, string s2, string s3) {
-        int n = s1.length();
-        int l = s2.length();
-        int m = s3.length();
-        // vector<vector<int>> dp(n+1, vector<int>(l+1, 0));
-        vector<int> prev(l + 1, 0), curr(l + 1, 0);
-       
-        if(n + l != m)return false;
-        // return solve(n-1, l-1, m-1, s1, s2, s3, dp);
-        prev[0] = 1;
-        curr[0] = 1;
-        for(int ind1 = 0; ind1 <=n; ind1++){
-            for(int ind2 = 0; ind2 <= l; ind2++){
-                int result = 0;
-                if(ind1 == 0 && ind2 == 0){
-                     continue;
-                }else if(ind1 > 0 && ind2 > 0 && s1[ind1 - 1] == s3[ind1 + ind2 - 1] && s2[ind2 - 1] == s3[ind1 + ind2 - 1]){
-                    result = (prev[ind2] || curr[ind2-1]);
-                }else if(ind1 > 0 && s1[ind1-1] == s3[ind1 + ind2 -1]){
-                    result = prev[ind2];
-                }else if(ind2 > 0 && s2[ind2-1] == s3[ind1 + ind2 - 1]){
-                    result = curr[ind2-1];
-                }
-                curr[ind2] = result;
-            }
-            prev = curr;
-        }
-        return prev[l];
+                int n1 = s1.length();
+                int n2 = s2.length();
+                int n3 = s3.length();
+                if(n1 + n2 != n3)return false;
+                vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
+                return solve(n1, n2, n3, s1, s2, s3, dp);
     }
 };
