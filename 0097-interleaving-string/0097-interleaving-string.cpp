b74f5c4fb9a81e6bdd9a7dc648b04906037e3b1b
Sahil Grewal <sahilgrewalhere2001@outlook.com>
2025-05-28 18:06:29 -0500

    Time: 6 ms (31.26%), Space: 9.8 MB (34.98%) - LeetHub

--- 0097-interleaving-string/0097-interleaving-string.cpp
+++ 0097-interleaving-string/0097-interleaving-string.cpp
@@ -18,11 +18,43 @@ class Solution {
 
 public:
     bool isInterleave(string s1, string s2, string s3) {
-                int n1 = s1.length();
-                int n2 = s2.length();
-                int n3 = s3.length();
-                if(n1 + n2 != n3)return false;
-                vector<vector<int>> dp(n1 + 1, vector<int>(n2 + 1, -1));
-                return solve(n1, n2, n3, s1, s2, s3, dp);
+                int n1x = s1.length();
+                int n2x = s2.length();
+                int n3x = s3.length();
+                if(n1x + n2x != n3x)return false;
+                vector<vector<int>> dp(n1x + 1, vector<int>(n2x + 1, 0));
+                // return solve(n1, n2, n3, s1, s2, s3, dp);
+                int n3 = 0;
+                for(int n1 = 0; n1 <= n1x; n1++){
+                    for(int n2 = 0; n2 <= n2x; n2++){
+                        n3 = n1 + n2;
+                        if(n3 == 0 && (n1 == 0 && n2 == 0)){
+                            dp[n1][n2] = true;
+                            continue;
+                        }
+                        if(n3 > 0 && (n1 == 0 && n2 == 0)){
+                            dp[n1][n2] = false;
+                            continue;
+                        }
+                        if(n3 == 0 && (n1 > 0 || n2 > 0)){
+                            dp[n1][n2] = false;
+                            continue;
+                        }
+                        
+                        int take1 = 0;
+                        int take2 = 0;
+                        if(n1 > 0 && n2 > 0 && n3 > 0 && (s1[n1 - 1] == s3[n3 - 1] && s2[n2-1] == s3[n3-1])){
+                            dp[n1][n2] = dp[n1 - 1][n2] || dp[n1][n2-1];
+                            continue;
+                        }else if(n1 > 0 && n3 > 0 && (s1[n1-1] == s3[n3-1])){
+                            take1 = dp[n1-1][n2];
+                        }else if(n2 > 0 && n3 > 0 && (s2[n2-1] == s3[n3-1])){
+                            take2 = dp[n1][n2-1];
+                        }
+                        dp[n1][n2] = take1 || take2;
+                        
+                    }
+                }
+                return dp[n1x][n2x];
     }
 };
